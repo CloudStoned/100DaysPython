eb0a916b623e95124f4ac12e2fc2eb172550c3ba
CloudStoned <cloudneo0719@gmail.com>
2024-07-07 18:33:30 +0800

    Add: Snake Game

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,7 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
-
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -18,12 +21,20 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1) 
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
+        food.refresh()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 
