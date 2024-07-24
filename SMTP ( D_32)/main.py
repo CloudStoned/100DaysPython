57b9c2ae51225a145d36c667981d6b07106b3cef
CloudStoned <cloudneo0719@gmail.com>
2024-07-24 17:15:56 +0800

    Update: variables

--- SMTP ( D_32)/main.py
+++ SMTP ( D_32)/main.py
@@ -1,10 +1,10 @@
 import smtplib
 
-my_email = "cloudbagtas@gmail.com"
-password = "+Pandora100104"
+my_email = ""
+password = ""
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="cloudneo0719@gmail.com", msg="Hello from cloudbagtas@gmail.com")
+connection.sendmail(from_addr=my_email, to_addrs="cloudneo0719@gmail.com", msg="Hello")
 connection.close()
