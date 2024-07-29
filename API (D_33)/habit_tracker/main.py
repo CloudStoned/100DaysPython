75f211dbd150e1f771aa8e71b89a83dd0eb83461
CloudStoned <cloudneo0719@gmail.com>
2024-07-29 21:00:08 +0800

    Update: Functions for request

--- API (D_33)/habit_tracker/main.py
+++ API (D_33)/habit_tracker/main.py
@@ -5,67 +5,81 @@ pixela_endpoint = "https://pixe.la/v1/users"
 TOKEN = "+Qaz123_WSX456"
 USERNAME = "cloud-100-104"
 GRAPH_ID = "graph1"
+HEADERS = { "X-USER-TOKEN": TOKEN}
 
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
+def create_account(TOKEN,USERNAME):
+    user_params = {
+        "token": TOKEN,
+        "username": USERNAME,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes"
+    }
 
-}
+    response = requests.post(url=pixela_endpoint, json=user_params)
 
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
+    return response
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+# print(create_account(TOKEN,USERNAME))
 
-headers ={
-    "X-USER-TOKEN": TOKEN
-}
+def create_graph(HEADERS):
+    graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+    graph_config = {
+        "id": GRAPH_ID,
+        "name": "Coding Graph",
+        "unit": "commit",
+        "type": "int",
+        "color": "shibafu"
+    }
+    response = requests.post(url=graph_endpoint,json=graph_config, headers=HEADERS)
 
-graph_config = {
-    "id": GRAPH_ID,
-    "name": "Coding Graph",
-    "unit": "commit",
-    "type": "int",
-    "color": "shibafu"
-}
+    return response
 
-# response = requests.post(url=graph_endpoint,json=graph_config, headers=headers)
-# print(response.text)
+# print(create_graph(HEADERS))
 
-today = datetime.now()
-# today = datetime(year=2024, month=7, day=28)
-# print(today.strftime("%Y%m%d"))
+def post_pixel(HEADERS):
+    today = datetime.now()
+    # today = datetime(year=2024, month=7, day=28)
+    # print(today.strftime("%Y%m%d"))
 
-pixel_post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+    pixel_post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
-post_habit = {
-    "date": today.strftime("%Y%m%d"),
-    "quantity": input("How many commits did you do today?: "),
-}
+    post_habit = {
+        "date": today.strftime("%Y%m%d"),
+        "quantity": input("How many commits did you do today?: "),
+    }
 
+    response = requests.post(url=pixel_post_endpoint, 
+                            json=post_habit, 
+                            headers=HEADERS)
 
-# response = requests.post(url=pixel_post_endpoint, 
-#                          json=post_habit, 
-#                          headers=headers)
-# print(response.text)
+    return response
 
-update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+print(post_pixel(HEADERS))
 
-update_habit = {
-    "quantity": "5"
-}
+def update_pixel(HEADERS):
+    update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
 
-# response = requests.put(url=update_endpoint,
-#                         json=update_habit,
-#                         headers=headers)
-# print(response.text)
+    update_habit = {
+        "quantity": "5"
+    }
 
-delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
-response = requests.delete(url=delete_endpoint,headers=headers)
-print(response.text)
+    response = requests.put(url=update_endpoint,
+                            json=update_habit,
+                            headers=HEADERS)
+    
+print(update_pixel(HEADERS))
 
+def delete_pixel(HEADERS):
+    today = datetime.now()
+    # today = datetime(year=2024, month=7, day=28)
+    # print(today.strftime("%Y%m%d"))
+
+    delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+    response = requests.delete(url=delete_endpoint,headers=HEADERS)
+
+    return response
+
+print(delete_pixel(HEADERS))
 
 
 
