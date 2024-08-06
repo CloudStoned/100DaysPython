e2f6c0d2607bdf80945e894577dcf2a4d6c416d9
CloudStoned <cloudneo0719@gmail.com>
2024-08-06 11:29:47 +0800

    Update SMTP

--- SMTP/main.py
+++ SMTP/main.py
@@ -1,10 +1,25 @@
 import smtplib
+from dotenv import load_dotenv
+import os
+import spotipy
 
-my_email = ""
-password = ""
+load_dotenv()
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="cloudneo0719@gmail.com", msg="Hello")
-connection.close()
+
+load_dotenv()
+
+email_sender = os.environ["email_sender"]
+app_password =  os.environ["app_password"]
+email_reciever = os.environ["email_reciever"]
+
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_sender, app_password)
+    server.sendmail(email_sender, email_reciever, "TEST MAIL")
+    print('Mail Sent')
+
+except smtplib.SMTPAuthenticationError as e:
+    print(f"Failed to send email: {e}")
+finally:
+    server.quit()
